6e04e49474d3b7d0c3aa02c62bddcdc872a7dc62
blacksnowmartin <martinmwendwa005@gmail.com>
2024-10-11 16:00:50 +0300

    added date option

--- Web/script.js
+++ Web/script.js
@@ -3,6 +3,8 @@ function submitForm() {
     const destination = document.getElementById("destination").value;
     const purpose = document.getElementById("purpose").value;
     const modeOfTravel = document.getElementById("modeOfTravel").value;
+    const date = document.getElementById("date").value;
+
 
     const recordsBody = document.getElementById("recordsBody");
     const newRow = recordsBody.insertRow(-1);
@@ -11,9 +13,11 @@ function submitForm() {
     const cell2 = newRow.insertCell(1);
     const cell3 = newRow.insertCell(2);
     const cell4 = newRow.insertCell(3);
+    const cell5 = newRow.insertCell(4);
 
     cell1.textContent = visitorName;
     cell2.textContent = destination;
     cell3.textContent = purpose;
     cell4.textContent = modeOfTravel;
+    cell5.textContent = date;
 }
